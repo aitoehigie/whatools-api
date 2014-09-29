b9d57562275a46162ec2d713e950593a76c94e4d
Adán Sánchez de Pedro Crespo <adansdpc@waalt.com>
2014-09-29 12:20:13 +0200

    Método lineTest

--- api.py
+++ api.py
@@ -6,12 +6,13 @@ from Yowsup.Common.debugger import Debugger
 from Yowsup.Common.constants import Constants
 from Examples.EchoClient import WhatsappEchoClient
 from Examples.GetClient import WhatsappGetClient
+from Examples.TestClient import WhatsappTestClient
 from Yowsup.Contacts.contacts import WAContactsSyncRequest
 from Yowsup.Registration.v2.coderequest import WACodeRequest
 from Yowsup.Registration.v2.existsrequest import WAExistsRequest
 from Yowsup.Registration.v2.regrequest import WARegRequest
 
-client = MongoClient()
+client = MongoClient('mongodb://192.168.1.2')
 db = client.waapi
 
 Users = db.users
@@ -71,7 +72,7 @@ def messages_post():
     res["error"] = "no-key"
   return res
 
-@route("/line/validate", method="GET")
+@route("/line/coderequest", method="GET")
 def line_validate():
   res = {"success": False}
   pn = request.params.pn
@@ -84,6 +85,53 @@ def line_validate():
       res["result"] = wa.send()
     else:
       res["error"] = "could-not-send"
+  else:
+    res["error"] = "bad-param"
+  return res
+
+@route("/line/regrequest", method="GET")
+def line_validate():
+  res = {"success": False}
+  pn = request.params.pn
+  cc = request.params.cc
+  code = request.params.code
+  if pn and cc and code:
+    wa = WARegRequest(cc, pn, code, Utilities.processIdentity(""))
+    if wa:
+      res["success"] = True
+      res["result"] = wa.register()
+    else:
+      res["error"] = "could-not-register"
+  else:
+    res["error"] = "bad-param"
+  return res
+  
+@route("/line/test", method="GET")
+def line_validate():
+  res = {"success": False}
+  lId = request.params.id
+  if lId:
+    line = Lines.find_one({"_id": lId});
+    if line:
+      wa = WhatsappTestClient();
+      if wa:
+        user = line["cc"] + line["pn"]
+        try:
+          pw = base64.b64decode(bytes(line["pass"].encode('utf-8')))
+        except TypeError:
+          res["error"] = "password-type-error"
+          Lines.update({"_id": lId}, {"$set": {"validated": "wrong"}});
+          return res
+        res["success"] = True
+        res["result"] = wa.login(user, pw)
+        if (res["result"] == "valid"):
+          Lines.update({"_id": lId}, {"$set": {"validated": True}})
+        else:
+          Lines.update({"_id": lId}, {"$set": {"validated": "wrong"}});
+      else:
+        res["error"] = "could-not-connect"
+    else:
+      res["error"] = "no-such-line"
   else:
     res["error"] = "bad-param"
   return res
