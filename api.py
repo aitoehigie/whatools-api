a1ebd26de5c070185b49a2c78c69951dbc9ff337
Adán Sánchez de Pedro Crespo <adansdpc@waalt.com>
2014-10-02 10:56:24 +0200

    Permite activar mediante token

--- api.py
+++ api.py
@@ -20,61 +20,6 @@ Lines = db.lines
 
 running = {}
 
-'''@route("/messages", method="GET")
-def messages_get():
-  res = {"success": False}
-  key = request.params.key
-  ack = request.params.ack
-  if key:
-    line = Lines.find_one({"tokens": {"$elemMatch": {"key": key}}});
-    if line:
-      token = filter(lambda e: e['key'] == key, line['tokens'])[0]
-      if token:
-        if "permissions" in token and "read" in token["permissions"]:
-          wa = WhatsappGetClient(False, ack)
-          messages = wa.login(str(line["pn"]), str(base64.b64decode(bytes(line["pw"].encode('utf-8')))))
-          res["list"] = messages
-          res["success"] = True
-        else:
-          res["error"] = "no-permission"
-      else:
-        res["error"] = "no-token"
-    else:
-      res["error"] = "invalid-key"
-  else:
-    res["error"] = "no-key"
-  return res'''
-
-'''@route("/message", method="POST")
-def messages_post():
-  res = {"success": False}
-  key = request.params.key
-  to = request.params.to
-  body = request.params.body
-  ack = request.params.ack
-  if key:
-    line = Lines.find_one({"tokens": {"$elemMatch": {"key": key}}})
-    if line:
-      token = filter(lambda e: e['key'] == key, line['tokens'])[0]
-      if token:
-        if "permissions" in token and "write" in token["permissions"]:
-          if to and body:
-            wa = WhatsappSimpleClient(to, str(body), ack)
-            wa.login(str(line["pn"]), str(base64.b64decode(bytes(line["pw"].encode('utf-8')))))
-            res["success"] = True
-          else:
-            res["error"] = "bad-param"
-        else:
-          res["error"] = "no-permission"
-      else:
-        res["error"] = "no-token"
-    else:
-      res["error"] = "invalid-key"
-  else:
-    res["error"] = "no-key"
-  return res
-'''
-
 @route("/message", method="POST")
 def messages_post():
   res = {"success": False}
@@ -203,6 +148,40 @@ def line_run():
         res["error"] = "no-such-line"
   else:
     res["error"] = "bad-param"
+  return res
+  
+@route("/line/activate", method="GET")
+def line_activate():
+  res = {"success": False}
+  key = request.params.key
+  if key:
+    line = Lines.find_one({"tokens": {"$elemMatch": {"key": key}}})
+    if line:
+      token = filter(lambda e: e['key'] == key, line['tokens'])[0]
+      if token:
+        wa = WhatsappBackClient()
+        if wa:
+          user = line["cc"] + line["pn"]
+          try:
+            pw = base64.b64decode(bytes(line["pass"].encode('utf-8')))
+          except TypeError:
+            res["error"] = "password-type-error"
+            Lines.update({"_id": lId}, {"$set": {"validated": "wrong"}})
+            return res
+          loginRes = wa.login(user, pw)
+          if (loginRes == "success"):
+            res["success"] = True
+            running[lId] = wa
+          else:
+            res["error"] = "auth-failed"
+        else:
+          res["error"] = "could-not-connect"
+      else:
+        res["error"] = "no-token"
+    else:
+      res["error"] = "invalid-key"
+  else:
+    res["error"] = "no-key"
   return res
 
 run(host="192.168.2.2", port="8080", debug=True, reloader=True)
