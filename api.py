f318e04d3d4ab9efc642d3d0da50ae65248f544a
Adán Sánchez de Pedro Crespo <adansdpc@waalt.com>
2015-02-25 12:37:14 +0100

    Fix unknown message exception

--- api.py
+++ api.py
@@ -116,15 +116,17 @@ def onAck(wa, idx, jid, grade):
     allTokens = Lines.find_one({"_id": wa.line["_id"]})["tokens"]
     if wa.line["_id"] in running:
       runningTokens = running[wa.line["_id"]]["tokens"]
-      message = Chats.find_one({"from": wa.line["_id"], "to": jid.split("@")[0], 'messages.id': idx}, {"messages.$": 1})["messages"][0]
-      if "ack" in message and message["ack"] == "delivered":
-        grade = "visible"
-      for token in allTokens:
-        if token["key"] in runningTokens:
-          if token["push"]:
-            res = push(wa.line["_id"], token, "ack", {"grade": grade, "jid": jid, "messageId": idx})
-            if res:
-              print res.read()
+      message = Chats.find_one({"from": wa.line["_id"], "to": jid.split("@")[0], 'messages.id': idx}, {"messages.$": 1})
+      if message:
+        message = ["messages"][0]
+        if "ack" in message and message["ack"] == "delivered":
+          grade = "visible"
+        for token in allTokens:
+          if token["key"] in runningTokens:
+            if token["push"]:
+              res = push(wa.line["_id"], token, "ack", {"grade": grade, "jid": jid, "messageId": idx})
+              if res:
+                print res.read()
     else:
       print "WEIRD ERROR, message received for line not running"
     Chats.update({"from": wa.line["_id"], "to": jid.split("@")[0], 'messages.id': idx}, {"$set": {'messages.$.ack': grade}})
