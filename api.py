2efe62975078f68bf9fa1eb68a565288ad786cb0
Ismael Maldonado <ismaelmg@waalt.com>
2015-04-16 12:45:45 +0200

    mensaje nuevo en conversación exitente archivada a inbox

--- api.py
+++ api.py
@@ -250,7 +250,7 @@ def onMediaReceived(wa, messageId, jid, participant, caption, type, preview, url
     msg["participant"] = participant
   if chat:
     # Push it to db
-    Chats.update({"from": wa.line["_id"], "to": to}, {"$push": {"messages": msg}, "$set": {"lastStamp": stamp}, "$inc": {"unread": 1}});
+    Chats.update({"from": wa.line["_id"], "to": to}, {"$set": {"folder": "inbox"}, "$push": {"messages": msg}, "$set": {"lastStamp": stamp}, "$inc": {"unread": 1}});
   else:
     # Create new chat
     Chats.insert({
@@ -301,7 +301,7 @@ def onMessageReceived(wa, messageId, jid, participant, messageContent, timestamp
     msg["broadcast"] = broadcast
   if chat:
     # Push it to db
-    Chats.update({"from": wa.line["_id"], "to": to}, {"$push": {"messages": msg}, "$set": {"lastStamp": stamp}, "$inc": {"unread": 1}})
+    Chats.update({"from": wa.line["_id"], "to": to}, {"$set": {"folder": "inbox"}, "$push": {"messages": msg}, "$set": {"lastStamp": stamp}, "$inc": {"unread": 1}})
   else:
     alias = False if participant else (pushName or False)
     # Create new chat
