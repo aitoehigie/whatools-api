bf9839d56ffeaf437f3d4921f40fc97c304d8207
Adán Sánchez de Pedro Crespo <adansdpc@waalt.com>
2015-04-24 13:25:29 +0200

    Don't recover deleted lines

--- api.py
+++ api.py
@@ -1002,5 +1002,5 @@ def reference():
 def index():
   return static_file('reference.htm', './static')
 
-recover(list(Lines.find({"tokens.active": True, "api": v}, {"tokens.$": 1})))
+recover(list(Lines.find({"tokens.active": True, "api": v, "deleted": {"$in": [None, False]}}, {"tokens.$": 1})))
 run(host="127.0.0.1", port="8081", server='gevent')
