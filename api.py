d6566042176e6f1cc07a00bde6ef119af2f5d81c
Adán Sánchez de Pedro Crespo <pedrocrespo@kadede.com>
2015-05-11 12:20:29 +0200

    action_forward and action_post

--- api.py
+++ api.py
@@ -101,7 +101,7 @@ def botify(wa, msg, pn):
               print pushRes.read()
               
   def action_forward(msg, payload):
-    to = payload
+    to = phoneFormat(wa.line["cc"], payload)
     body = "♻ +%s:\n%s" % (msg["from"], msg["body"])
     stamp = msg["stamp"]
     chat = Chats.find_one({"from": wa.line["_id"], "to": to})
@@ -131,12 +131,60 @@ def botify(wa, msg, pn):
           pushRes = push(wa.line["_id"], token, "carbon", {"messageId": msgId, "jid": to, "messageContent": body, "timestamp": stamp})
           if pushRes:
             print pushRes.read()
-      
+            
+  def action_post(msg, payload):
+    to = msg["from"]
+    url = payload
+    stamp =  long(time.time()*1000)
+    chat = Chats.find_one({"from": wa.line["_id"], "to": to})
+    data = {
+      "body": msg["body"]
+    }
+    params = urllib.urlencode(data)
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+    if url.split("://")[0] == "https":
+      conn = httplib.HTTPSConnection(url.split("/")[2], 443)
+    else:
+      conn = httplib.HTTPConnection(url.split("/")[2], 80)
+    try:
+      conn.request("POST", '/' + "/".join(url.split("/")[3:]), params, headers)
+      res = conn.getresponse()
+    except:
+      pass
+    else:
+      answer = res.read()
+      msgId = wa.call("message_send", (to, messageSign(answer, wa.line)))
+      msg = {
+        "id": msgId,
+        "mine": True,
+        "body": answer,
+        "stamp": stamp,
+        "ack": "sent"
+      }
+      if chat:
+        Chats.update({"from": wa.line["_id"], "to": to}, {"$set": {"folder": "inbox"}, "$push": {"messages": msg}, "$set": {"lastStamp": stamp}})
+      else:
+        Chats.insert({
+          "_id": str(objectid.ObjectId()),
+          "from": wa.line["_id"],
+          "to": to,
+          "messages": [msg],
+          "lastStamp": stamp,
+          "alias": False
+        })
+      runningTokens = running[wa.line["_id"]]["tokens"]
+      for token in wa.line["tokens"]:
+        if token["key"] in runningTokens:
+          if token["push"]:
+            pushRes = push(wa.line["_id"], token, "carbon", {"messageId": msgId, "jid": to, "messageContent": answer, "timestamp": stamp})
+            if pushRes:
+              print pushRes.read()
     
   actions = {
     'answer': action_answer,
     'canned': action_canned,
-    'forward': action_forward
+    'forward': action_forward,
+    'post': action_post
   }
   line = Lines.find_one({"_id": wa.line["_id"]})
   chat = Chats.find_one({"from": wa.line["_id"],  "to": pn})
