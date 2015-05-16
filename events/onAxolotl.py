07adf07089638f68cca13a3cb926d36880d86f03
Adán Sánchez de Pedro Crespo <pedrocrespo@kadede.com>
2015-05-16 13:19:24 +0200

    onAxolotl

--- events/onAxolotl.py
+++ events/onAxolotl.py
@@ -1,10 +1,13 @@
+import gevent
 from helpers import *
+from events import *
+from client.stack import YowsupAsyncStack
 
 def onAxolotlReady(wa):
   line = db.Lines.find_one({"_id": wa.line["_id"]})
-  def cb(wa, loginRes, payload):
-    if line["_id"] in running:
-      running[line["_id"]]["yowsup"] = wa
+  def cb(wa, loginRes, data = None):
+    if line["_id"] in g.running:
+      g.running[line["_id"]]["yowsup"] = wa
   newWa = YowsupAsyncStack([line["cc"] + line["pn"], line["pass"]], line, line["tokens"][0], eventHandler, logger, cb)
   if newWa:
     gevent.spawn(newWa.login)
