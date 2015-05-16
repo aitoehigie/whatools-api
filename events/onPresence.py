4f28290b6bbcc8f41d6aea564f63caa5b8cc6832
Adán Sánchez de Pedro Crespo <pedrocrespo@kadede.com>
2015-05-16 13:14:36 +0200

    Don't die if presence is denied

--- events/onPresence.py
+++ events/onPresence.py
@@ -1,6 +1,6 @@
 from helpers import *
 
 def onPresence(wa, _type, _from, last):
-  if last is not None:
+  if last is not None and last.isdigit():
     to = _from.split("@")[0]
     db.Chats.update({"to": to}, {"$set": {"lastSeen": int(last)}})
