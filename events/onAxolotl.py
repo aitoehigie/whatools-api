c481e90d7d4b39bb927a0ed6de94d81fde97c107
Adán Sánchez de Pedro Crespo <pedrocrespo@kadede.com>
2015-05-16 13:24:16 +0200

    onAxolotl

--- events/onAxolotl.py
+++ events/onAxolotl.py
@@ -8,6 +8,6 @@ def onAxolotlReady(wa):
   def cb(wa, loginRes, data = None):
     if line["_id"] in g.running:
       g.running[line["_id"]]["yowsup"] = wa
-  newWa = YowsupAsyncStack([line["cc"] + line["pn"], line["pass"]], line, line["tokens"][0], eventHandler, logger, cb)
+  newWa = YowsupAsyncStack([line["cc"] + line["pn"], line["pass"]], line, line["tokens"][0], events.eventHandler, logger, cb)
   if newWa:
     gevent.spawn(newWa.login)
