65afd7eed3737f2760d7e870172f24b8ea5c6813
Adán Sánchez de Pedro Crespo <adansdpc@waalt.com>
2015-05-28 19:35:23 +0200

    Perfect recovery and subscriptions :)

--- methods/unsubscribe.py
+++ methods/unsubscribe.py
@@ -15,7 +15,7 @@ class unsubscribeGetMethod(method):
     db.Lines.update({"_id": self.line["_id"], "tokens.key": self.token["key"]}, {"$set": {"tokens.$.active": False}})
     if self.token["key"] in self.running[self.line["_id"]]["tokens"]:
       self.running[self.line["_id"]]["tokens"].remove(self.token["key"])
-    if len(self.running[self.line["_id"]]["tokens"]) < 1:
+    if len(self.running[self.line["_id"]]["tokens"]) < 2:
       db.Lines.update({"_id": self.line["_id"]}, {"$set": {"reconnect": False, "active": False}})
       if self.line["_id"] in self.running:
         del self.running[self.line["_id"]]
