8ebaa9d871aeb9cc6e3f21eb60eaa44f0256294f
Adán Sánchez de Pedro Crespo <adansdpc@waalt.com>
2015-05-28 02:19:07 +0200

    Wait 10s before reconnect

--- events/onAuth.py
+++ events/onAuth.py
@@ -5,17 +5,18 @@ from client.stack import YowsupAsyncStack
 from client.layer import AsyncLayer
 
 def onAuthFailed(wa):
-  db.Lines.update({"_id": wa.line["_id"]}, {"$set": {"active": False, "reconnect": False, "valid": False}});
+  db.Lines.update({"_id": wa.line["_id"]}, {"$set": {"active": False, "reconnect": True, "valid": False}});
 
 def onAuthSuccess(wa):
   db.Lines.update({"_id": wa.line["_id"]}, {"$set": {"active": True, "reconnect": True, "valid": True}});
   
 def onDisconnected(wa, reason):
   print "???? DISCONNECTION", reason, wa.line["active"], wa.line["reconnect"], wa.line["_id"], wa.line["cc"], wa.line["pn"]
-  if reason != "Requested":
+  if reason != "Requested" and wa.line["reconnect"]:
     print "???? RECONNECTING!"
     @delay(10.0)
     def reconnect():
+      wa.line["reconnect"] = False
       line = db.Lines.find_one({"_id": wa.line["_id"]})
       def cb(wa, loginRes, data = None):
         if line["_id"] in g.running:
