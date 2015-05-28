1a5706b1530eff2981244efbf7eb0dc077252a84
Adán Sánchez de Pedro Crespo <adansdpc@waalt.com>
2015-05-28 11:18:30 +0200

    Testing reconnection

--- events/onAuth.py
+++ events/onAuth.py
@@ -14,10 +14,10 @@ def onDisconnected(wa, reason):
   print "???? DISCONNECTION", reason, wa.line["active"], wa.line["reconnect"], wa.line["_id"], wa.line["cc"], wa.line["pn"]
   if reason != "Requested" and wa.line["reconnect"]:
     print "???? RECONNECTING!"
-    line = db.Lines.find_one({"_id": wa.line["_id"]})
+    '''line = db.Lines.find_one({"_id": wa.line["_id"]})
     def cb(wa, loginRes, data = None):
       if line["_id"] in g.running:
         g.running[line["_id"]]["yowsup"] = wa
     newWa = YowsupAsyncStack(line, line["tokens"][0], wa.getProp(AsyncLayer.HANDLERS), logger, cb)
     if newWa:
-      gevent.spawn(newWa.login)
+      gevent.spawn(newWa.login)'''
