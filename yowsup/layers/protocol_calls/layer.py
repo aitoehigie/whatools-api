1bd064ca6370b6f81877e5472ed1f5156850be8a
Adán Sánchez de Pedro Crespo <adansdpc@waalt.com>
2015-05-28 13:38:30 +0200

    Testing reconnection

--- yowsup/layers/protocol_calls/layer.py
+++ yowsup/layers/protocol_calls/layer.py
@@ -21,9 +21,9 @@ class YowCallsProtocolLayer(YowProtocolLayer):
         entity = CallProtocolEntity.fromProtocolTreeNode(node)
         if entity.getType() == "offer":
             receipt = OutgoingReceiptProtocolEntity(node["id"], node["from"], callId = entity.getCallId())
-            #self.toLower(receipt.toProtocolTreeNode())
+            self.toLower(receipt.toProtocolTreeNode())
         else:
             ack = OutgoingAckProtocolEntity(node["id"], "call", None, node["from"])
-            #self.toLower(ack.toProtocolTreeNode())
+            self.toLower(ack.toProtocolTreeNode())
         self.toUpper(entity)
 
